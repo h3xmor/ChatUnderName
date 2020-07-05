645d7bb2357fd83af2e4cfdb5ecace712aae1cd5
h3xmor <67831288+h3xmor@users.noreply.github.com>
2020-07-05 13:02:26 +0430

    Update Main.php
    Some Clean Coding Changes

--- src/Hexmor/ChatUnderName/Main.php
+++ src/Hexmor/ChatUnderName/Main.php
@@ -19,17 +19,18 @@ class Main extends PluginBase implements Listener{
 	}
 
 	public function onChat(PlayerChatEvent $ev) {
-	$p = $ev->getPlayer();
-	$msg = $ev->getMessage();
-        $nameTag = $this->getServer()->getPluginManager()->getPlugin("PureChat");
-	if(!is_null($nameTag)){
+		$p = $ev->getPlayer();
+		$msg = $ev->getMessage();
+       		 $nameTag = $this->getServer()->getPluginManager()->getPlugin("PureChat");
+		if(!is_null($nameTag)){
 		
-		$p->setNameTag($nameTag->getNametag($p) . "\n " . mb_strimwidth($msg, 0, 30, "...") . "\n");
-	}else{
-	$current = $p->getNameTag();
-        $p->setNameTag($p->getName() . "\n " . mb_strimwidth($msg, 0, 30, "...") . "\n");
+			$p->setNameTag($nameTag->getNametag($p) . "\n " . mb_strimwidth($msg, 0, 30, "...") . "\n");
+		}else{
+			
+   		       $p->setNameTag($p->getName() . "\n " . mb_strimwidth($msg, 0, 30, "...") . "\n");
+		}
 	}
-}
+	
 	public function onDisable() : void{
 		$this->getLogger()->info("Goodbye .");
 	}
