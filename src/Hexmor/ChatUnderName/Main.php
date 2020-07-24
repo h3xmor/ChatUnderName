17b2e8d735bfb328d25e527affa0223159cd491d
h3xmor <67831288+h3xmor@users.noreply.github.com>
2020-07-25 00:04:23 +0430

    Update Main.php
    Clean Up And Removing OnEnable and OnDisable Messages .

--- src/Hexmor/ChatUnderName/Main.php
+++ src/Hexmor/ChatUnderName/Main.php
@@ -14,24 +14,18 @@ class Main extends PluginBase implements Listener{
 
 	public function onEnable() : void{
 		 $this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->getLogger()->info("Chat Under Name Active ! By Hexmor !");
-        $this->getLogger()->info("Discord : hexmor#3032");
 	}
 
-	public function onChat(PlayerChatEvent $ev) {
-		$p = $ev->getPlayer();
-		$msg = $ev->getMessage();
-       		 $nameTag = $this->getServer()->getPluginManager()->getPlugin("PureChat");
-		if(!is_null($nameTag)){
+	public function onChat(PlayerChatEvent $event) {
+		$player = $event->getPlayer();
+		$message = $ev->getMessage();
+       		 $purechat = $this->getServer()->getPluginManager()->getPlugin("PureChat");
+		if(!is_null($purechat)){
 		
-			$p->setNameTag($nameTag->getNametag($p) . "\n " . mb_strimwidth($msg, 0, 30, "...") . "\n");
+			$p->setNameTag($nameTag->getNametag($player) . "\n " . mb_strimwidth($msg, 0, 30, "..."));
 		}else{
 			
-   		       $p->setNameTag($p->getName() . "\n " . mb_strimwidth($msg, 0, 30, "...") . "\n");
+   		       $p->setNameTag($player->getName() . "\n " . mb_strimwidth($msg, 0, 30, "..."));
 		}
-	}
-	
-	public function onDisable() : void{
-		$this->getLogger()->info("Goodbye .");
 	}
 }
